004084a175711a147c65d644fce433fb1c9fe463
jaeyoungjang2 <lion85488@gmail.com>
2023-11-09 21:05:37 +0900

    feat: 뉴스 문제풀이

---
+++ src/main/kotlin/programmers/News17677_Others1.java
@@ -0,0 +1,53 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class News17677_Others1 {
+    private static final Integer MULTIPLIER = Character.MAX_VALUE + 1;
+
+    public int solution(String str1, String str2) {
+        String word1 = str1.toLowerCase();
+        String word2 = str2.toLowerCase();
+
+        Map<String, Long> words1 = group(word1);
+        Map<String, Long> words2 = group(word2);
+
+        Integer intersection = getIntersection(words1, words2);
+        Integer union = getUnion(words1, words2);
+
+        if (intersection.equals(union) && union.equals(0)) {
+            return MULTIPLIER;
+        }
+
+        return (int) (intersection.doubleValue() / union.doubleValue() * MULTIPLIER);
+    }
+
+    private Map<String, Long> group(String word) {
+        return IntStream.range(0, word.length() - 1)
+                .mapToObj(index -> word.substring(index, index + 2))
+                .filter(text -> text.chars().allMatch(character -> Character.isAlphabetic((char) character)))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    }
+
+    private Integer getIntersection(Map<String, Long> words1, Map<String, Long> words2) {
+        return words1.entrySet().stream()
+                .filter(entry -> words2.containsKey(entry.getKey()))
+                .map(entry -> Math.min(entry.getValue(), words2.get(entry.getKey())))
+                .mapToInt(Long::intValue)
+                .sum();
+    }
+
+    private Integer getUnion(Map<String, Long> words1, Map<String, Long> words2) {
+        Map<String, Long> copiedWords = new HashMap<>(words2);
+        words1.forEach((key, value) -> copiedWords.put(key, Math.max(value, words2.getOrDefault(key, 0L))));
+
+        return copiedWords.values().stream()
+                .mapToInt(Long::intValue)
+                .sum();
+
+    }
+}
